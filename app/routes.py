9fb51218c6074b7cd8d1e5e389d22479288812d7
Iqbal Tawakal <iqtwkl@gmail.com>
2022-05-24 17:13:32 +0700

    init(login): form login

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,15 @@
 from app import app
 from flask import render_template
+from .forms.login import Login as LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'name': 'World'}
     return render_template('index.html', title='Index', user=user)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
