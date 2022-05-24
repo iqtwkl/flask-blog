12b81df2bd4d5bcf938e08844efc9430f4fcce08
Iqbal Tawakal <iqtwkl@gmail.com>
2022-05-24 10:35:21 +0700

    fix(routes): change name to World

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,5 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'name': app.config['SECRET_KEY']}
+    user = {'name': 'World'}
     return render_template('index.html', title='Index', user=user)
