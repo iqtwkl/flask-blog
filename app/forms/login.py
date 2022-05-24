4ad074572f2a4c9dd1de03e67c336ff1e3b97735
Iqbal Tawakal <iqtwkl@gmail.com>
2022-05-24 17:23:51 +0700

    fix(login): submit button

--- app/forms/login.py
+++ app/forms/login.py
@@ -6,4 +6,4 @@ class Login(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
-    submit = StringField('Sign In')
+    submit = SubmitField('Sign In')
